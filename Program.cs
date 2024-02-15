eabc8165981f3c6cbdbe2fe9fd8100de6cee4b69
Ahmadxon <ahmadxonahmedov64@gmail.com>
2024-02-15 23:47:52 +0500

    Upload project

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿
+Console.WriteLine("Tub sonlarni aniqlovchi loyixa: "); 
+
+Console.Write("Enter a number: ");
+    string userInput = Console.ReadLine();
+    int userNumber = Convert.ToInt32(userInput);
+
+      for (int i = 2; i < userNumber; i++ )
+         {
+           int sum = 0;
+            for (int j = 1; j <= i; j++)
+            {
+                 if(i % j == 0) 
+                {
+                     sum++;
+                }
+                }
+                if(sum == 2)
+                {
+                    Console.WriteLine(i);
+                }
+            }
